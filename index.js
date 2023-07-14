c1ed97316ce19c1bde0112157e11a7b17a31dec9
Brianna Ponder <lemleybrianna123100@gmail.com>
2023-07-14 13:41:58 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    const hq = 42
+    if (blocks > 42) {
+        return blocks - hq
+    }
+    else {
+        return hq - blocks
+    }
+}
+
+function distanceFromHqInFeet(feet) {
+  return distanceFromHqInBlocks(feet) * 264
+}
+
+function distanceTravelledInFeet(start, end) {
+    if (end > start) {
+        return (end - start) * 264
+    }
+    else {
+        return (start - end) * 264
+    }
+}
+
+function calculatesFarePrice(start, end) {
+    let price = distanceTravelledInFeet(start, end)
+    if (price <= 400) {
+        return 0
+    }
+    else if (price >400 && price <= 2000) {
+        return ((price - 400) * 0.02) 
+    }
+    else if (price > 2000 && price <= 2500) {
+        return 25
+    }
+    else if (price > 2500) {
+        return 'cannot travel that far'
+    }
+}
